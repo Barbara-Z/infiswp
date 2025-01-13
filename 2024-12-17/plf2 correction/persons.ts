8582fe7e83543a3cb2ba55935786dbc66fd34866
Barbara Zalud <zal230117@spengergasse.at>
2025-01-13 20:00:01 +0100

    ...

--- 2024-12-17/plf2 correction/persons.ts
+++ 2024-12-17/plf2 correction/persons.ts
@@ -1,11 +1,11 @@
 import { readFileStr } from "https://deno.land/std@0.42.0/fs/mod.ts";
 const data = await readFileStr("persons.csv", { encoding: "utf-8" });
-const lines = data.split('\n');
+const lines = data.split("\n");
 for (const line of lines){
     const [firstName, lastName, gender, weight, height] = line.split('|');
     if (gender === 'female'){
         console.log(`${firstName} ${lastName} ist weiblich. Sie ist ${weight}kg schwer und ${height}m groß.`);
     }else {
         console.log(`${firstName} ${lastName} ist männlich. Er ist ${weight}kg schwer und ${height}m groß.`);
-    }
+    }   
 }
