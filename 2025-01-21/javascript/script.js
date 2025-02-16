6bac95d5a24eead5f5a0f30990db76c43fd35578
Barbara Zalud <zal230117@spengergasse.at>
2025-02-16 16:07:48 +0100

    swp hü

--- 2025-01-21/javascript/script.js
+++ 2025-01-21/javascript/script.js
@@ -1,4 +1,13 @@
+/*const liste = document.getElementById("liste");
 for (i = 1; i <= 10; i++) {
     console.log(i);
-    document.getElementById("liste").innerHTML+=`<li>${i}`;
+    const item = document.createElement("li");
+    item.innerHTML += `${i}`;
+    liste.appendChild(item);
+}*/
+
+
+for (i = 1; i <= 10; i++) {
+    console.log(i);
+    document.getElementById("liste").innerHTML += `<li>${i}`;
 }
